dd3be6153fb6faca50e1ff918e680744cc1d20b9
Sean Behan <codebam@riseup.net>
2024-05-14 11:43:27 -0400

    remove unused env vars

--- packages/worker/src/worker.ts
+++ packages/worker/src/worker.ts
@@ -3,24 +3,11 @@ import TelegramBot from '../../main/src/telegram_bot';
 
 export interface Environment {
 	SECRET_TELEGRAM_API_TOKEN: string;
-	KV_GET_SET: KVNamespace;
-	KV_UID_DATA: KVNamespace;
-
 	SECRET_TELEGRAM_API_TOKEN2: string;
-
 	SECRET_TELEGRAM_API_TOKEN3: string;
-
-	SECRET_TELEGRAM_API_TOKEN4: string;
-
-	SECRET_TELEGRAM_API_TOKEN5: string;
-
 	AI: Ai;
-
 	DB: D1Database;
-
 	R2: R2Bucket;
-
-	CHAT_MODEL: string;
 }
 
 type promiseFunc<T> = (resolve: (result: T) => void, reject: (e?: Error) => void) => Promise<T>;
