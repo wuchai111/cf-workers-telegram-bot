f1cb2720ffbbfda3a8eb55a0d03411e9bba9c5e2
Sean Behan <codebam@riseup.net>
2024-05-15 01:48:19 -0400

    refactor handle function

--- packages/main/src/telegram_bot.ts
+++ packages/main/src/telegram_bot.ts
@@ -27,48 +27,55 @@ export default class TelegramBot {
 
 	async handle(request: Request): Promise<Response> {
 		this.webhook = new Webhook(this.token, request);
-		if (request.method === 'POST') {
-			this.update = await request.json();
-		} else {
-			this.update = new TelegramUpdate({});
-		}
 		const url = new URL(request.url);
 		if (`/${this.token}` === url.pathname) {
-			switch (url.searchParams.get('command')) {
-				case 'set':
-					return this.webhook.set();
+			switch (request.method) {
+				case 'POST': {
+					this.update = await request.json();
+					console.log(this.update);
+					let command = 'default';
+					let args: string[] = [];
+					const ctx = new TelegramExecutionContext(this, this.update);
+					this.currentContext = ctx;
+					switch (ctx.update_type) {
+						case 'message': {
+							args = this.update.message?.text?.split(' ') ?? [];
+							break;
+						}
+						case 'inline': {
+							args = this.update.inline_query?.query.split(' ') ?? [];
+							break;
+						}
+						case 'photo': {
+							command = ':photo';
+							break;
+						}
+						default:
+							break;
+					}
+					if (args.at(0)?.startsWith('/')) {
+						command = args.at(0)?.slice(1) ?? 'default';
+					}
+					this.commands['any']?.(ctx);
+					if (!this.commands[command]) {
+						command = 'default';
+					}
+					return await this.commands[command]?.(ctx);
+				}
+				case 'GET': {
+					switch (url.searchParams.get('command')) {
+						case 'set':
+							return this.webhook.set();
+
+						default:
+							break;
+					}
+					break;
+				}
+
 				default:
 					break;
 			}
-			console.log(this.update);
-			let command = 'default';
-			let args: string[] = [];
-			const ctx = new TelegramExecutionContext(this, this.update);
-			this.currentContext = ctx;
-			switch (ctx.update_type) {
-				case 'message': {
-					args = this.update.message?.text?.split(' ') ?? [];
-					break;
-				}
-				case 'inline': {
-					args = this.update.inline_query?.query.split(' ') ?? [];
-					break;
-				}
-				case 'photo': {
-					command = ':photo';
-					break;
-				}
-				default:
-					break;
-			}
-			if (args.at(0)?.startsWith('/')) {
-				command = args.at(0)?.slice(1) ?? 'default';
-			}
-			this.commands['any']?.(ctx);
-			if (!this.commands[command]) {
-				command = 'default';
-			}
-			return await this.commands[command]?.(ctx);
 		}
 		return new Response('ok');
 	}
