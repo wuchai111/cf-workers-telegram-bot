44a3e1ae681eea1847cb27fca0675ac569ba254a
Sean Behan <codebam@riseup.net>
2024-05-15 13:34:25 -0400

    add default export

--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -2,4 +2,5 @@ import TelegramExecutionContext from './ctx';
 import TelegramBot from './telegram_bot';
 import * as Types from './types';
 
+export default TelegramBot;
 export { TelegramBot, Types, TelegramExecutionContext };
