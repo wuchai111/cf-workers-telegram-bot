7c60e533c4c2e6762028c0d0c002521235ad3a75
Sean Behan <codebam@riseup.net>
2024-09-03 11:37:08 -0400

    switch to llama 3.1

--- packages/worker/src/worker.ts
+++ packages/worker/src/worker.ts
@@ -223,7 +223,7 @@ export default {
 							let response: AiTextGenerationOutput;
 							try {
 								// @ts-expect-error broken bindings
-								response = await env.AI.run('@cf/meta/llama-3-8b-instruct', { messages, max_tokens: 150 });
+								response = await env.AI.run('@cf/meta/llama-3.1-8b-instruct', { messages, max_tokens: 150 });
 							} catch (e) {
 								console.log(e);
 								await bot.reply(`Error: ${e as string}`);
@@ -254,7 +254,7 @@ export default {
 							let response: AiTextGenerationOutput;
 							try {
 								// @ts-expect-error broken bindings
-								response = await env.AI.run('@cf/meta/llama-3-8b-instruct', { messages, max_tokens: 100 });
+								response = await env.AI.run('@cf/meta/llama-3.1-8b-instruct', { messages, max_tokens: 100 });
 							} catch (e) {
 								console.log(e);
 								await bot.reply(`Error: ${e as string}`);
