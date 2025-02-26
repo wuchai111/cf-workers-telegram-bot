d8b632a599a07edbc1ecdc3f2d3f0c30d89ff119
Sean Behan <codebam@riseup.net>
2025-02-26 14:14:00 -0500

    update telegram_execution_context with augment

--- packages/main/src/telegram_execution_context.ts
+++ packages/main/src/telegram_execution_context.ts
@@ -7,193 +7,215 @@ import TelegramInlineQueryResultVideo from './types/TelegramInlineQueryResultVid
 
 /** Class representing the context of execution */
 export default class TelegramExecutionContext {
-	/** an instance of the telegram bot */
-	bot: TelegramBot;
-	/** an instance of the telegram update */
-	update: TelegramUpdate;
-	/** string representing the type of update that was sent */
-	update_type = '';
-	/** reference to TelegramApi class */
-	api = new TelegramApi();
+  /** an instance of the telegram bot */
+  bot: TelegramBot;
+  /** an instance of the telegram update */
+  update: TelegramUpdate;
+  /** string representing the type of update that was sent */
+  update_type = '';
+  /** reference to TelegramApi class */
+  api = new TelegramApi();
 
-	/**
-	 * Create a telegram execution context
-	 * @param bot - the telegram bot
-	 * @param update - the telegram update
-	 */
-	constructor(bot: TelegramBot, update: TelegramUpdate) {
-		this.bot = bot;
-		this.update = update;
+  /**
+   * Create a telegram execution context
+   * @param bot - the telegram bot
+   * @param update - the telegram update
+   */
+  constructor(bot: TelegramBot, update: TelegramUpdate) {
+    this.bot = bot;
+    this.update = update;
 
-		if (this.update.message?.photo) {
-			this.update_type = 'photo';
-		} else if (this.update.message?.text) {
-			this.update_type = 'message';
-		} else if (this.update.inline_query?.query) {
-			this.update_type = 'inline';
-		} else if (this.update.message?.document) {
-			this.update_type = 'document';
-		} else if (this.update.callback_query?.id) {
-			this.update_type = 'callback';
-		} else if (this.update.business_message) {
-			this.update_type = 'business_message';
-		}
-	}
+    this.update_type = this.determineUpdateType();
+  }
 
-	/**
-	 * Reply to the last message with a video
-	 * @param video - string to a video on the internet or a file_id on telegram
-	 * @param options - any additional options to pass to sendVideo
-	 */
-	async replyVideo(video: string, options: Record<string, number | string | boolean> = {}) {
-		switch (this.update_type) {
-			case 'message':
-				return await this.api.sendVideo(this.bot.api.toString(), {
-					...options,
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					reply_to_message_id: this.update.message?.message_id.toString() ?? '',
-					video,
-				});
-			case 'inline':
-				return await this.api.answerInline(this.bot.api.toString(), {
-					...options,
-					inline_query_id: this.update.inline_query?.id.toString() ?? '',
-					results: [new TelegramInlineQueryResultVideo(video)],
-				});
+  /**
+   * Determine the type of update received
+   * @returns The update type as a string
+   */
+  private determineUpdateType(): string {
+    if (this.update.message?.photo) {
+      return 'photo';
+    } else if (this.update.message?.text) {
+      return 'message';
+    } else if (this.update.inline_query?.query) {
+      return 'inline';
+    } else if (this.update.message?.document) {
+      return 'document';
+    } else if (this.update.callback_query?.id) {
+      return 'callback';
+    } else if (this.update.business_message) {
+      return 'business_message';
+    }
+    return '';
+  }
 
-			default:
-				break;
-		}
-	}
+  /**
+   * Get the chat ID from the current update
+   * @returns The chat ID as a string or empty string if not available
+   */
+  private getChatId(): string {
+    if (this.update.message?.chat.id) {
+      return this.update.message.chat.id.toString();
+    } else if (this.update.business_message?.chat.id) {
+      return this.update.business_message.chat.id.toString();
+    }
+    return '';
+  }
 
-	/**
-	 * Get File from telegram file_id
-	 * @param file_id - telegram file_id
-	 */
-	async getFile(file_id: string) {
-		return await this.api.getFile(this.bot.api.toString(), { file_id }, this.bot.token);
-	}
+  /**
+   * Get the message ID from the current update
+   * @returns The message ID as a string or empty string if not available
+   */
+  private getMessageId(): string {
+    return this.update.message?.message_id.toString() ?? '';
+  }
 
-	/**
-	 * Reply to the last message with a photo
-	 * @param photo - url or file_id to photo
-	 * @param caption - photo caption
-	 * @param options - any additional options to pass to sendPhoto
-	 */
-	async replyPhoto(photo: string, caption = '', options: Record<string, number | string | boolean> = {}) {
-		switch (this.update_type) {
-			case 'photo':
-				return await this.api.sendPhoto(this.bot.api.toString(), {
-					...options,
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					reply_to_message_id: this.update.message?.message_id.toString() ?? '',
-					photo,
-					caption,
-				});
-			case 'message':
-				return await this.api.sendPhoto(this.bot.api.toString(), {
-					...options,
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					reply_to_message_id: this.update.message?.message_id.toString() ?? '',
-					photo,
-					caption,
-				});
-			case 'inline':
-				return await this.api.answerInline(this.bot.api.toString(), {
-					inline_query_id: this.update.inline_query?.id.toString() ?? '',
-					results: [new TelegramInlineQueryResultPhoto(photo)],
-				});
+  /**
+   * Reply to the last message with a video
+   * @param video - string to a video on the internet or a file_id on telegram
+   * @param options - any additional options to pass to sendVideo
+   * @returns Promise with the API response
+   */
+  async replyVideo(video: string, options: Record<string, number | string | boolean> = {}) {
+    switch (this.update_type) {
+      case 'message':
+        return await this.api.sendVideo(this.bot.api.toString(), {
+          ...options,
+          chat_id: this.getChatId(),
+          reply_to_message_id: this.getMessageId(),
+          video,
+        });
+      case 'inline':
+        return await this.api.answerInline(this.bot.api.toString(), {
+          ...options,
+          inline_query_id: this.update.inline_query?.id.toString() ?? '',
+          results: [new TelegramInlineQueryResultVideo(video)],
+        });
 
-			default:
-				break;
-		}
-	}
+      default:
+        return null;
+    }
+  }
 
-	/**
-	 * Send typing in a chat
-	 */
-	async sendTyping() {
-		switch (this.update_type) {
-			case 'message':
-				return await this.api.sendChatAction(this.bot.api.toString(), {
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					action: 'typing',
-				});
-			case 'business_message':
-				return await this.api.sendChatAction(this.bot.api.toString(), {
-					business_connection_id: this.update.business_message?.business_connection_id.toString(),
-					chat_id: this.update.business_message?.chat.id.toString() ?? '',
-					action: 'typing',
-				});
-			default:
-				break;
-		}
-	}
+  /**
+   * Get File from telegram file_id
+   * @param file_id - telegram file_id
+   * @returns Promise with the file response
+   */
+  async getFile(file_id: string) {
+    return await this.api.getFile(this.bot.api.toString(), { file_id }, this.bot.token);
+  }
 
-	/**
-	 * Reply to an inline message with a title and content
-	 * @param title - title to reply with
-	 * @param message - message contents to reply with
-	 * @param parse_mode - parse mode to use
-	 */
-	async replyInline(title: string, message: string, parse_mode = '') {
-		switch (this.update_type) {
-			case 'inline':
-				return await this.api.answerInline(this.bot.api.toString(), {
-					inline_query_id: this.update.inline_query?.id.toString() ?? '',
-					results: [new TelegramInlineQueryResultArticle({ content: message, title, parse_mode })],
-				});
-			default:
-				break;
-		}
-	}
+  /**
+   * Reply to the last message with a photo
+   * @param photo - url or file_id to photo
+   * @param caption - photo caption
+   * @param options - any additional options to pass to sendPhoto
+   * @returns Promise with the API response
+   */
+  async replyPhoto(photo: string, caption = '', options: Record<string, number | string | boolean> = {}) {
+    switch (this.update_type) {
+      case 'photo':
+      case 'message':
+        return await this.api.sendPhoto(this.bot.api.toString(), {
+          ...options,
+          chat_id: this.getChatId(),
+          reply_to_message_id: this.getMessageId(),
+          photo,
+          caption,
+        });
+      case 'inline':
+        return await this.api.answerInline(this.bot.api.toString(), {
+          inline_query_id: this.update.inline_query?.id.toString() ?? '',
+          results: [new TelegramInlineQueryResultPhoto(photo)],
+        });
 
-	/**
-	 * Reply to the last message with text
-	 * @param message - text to reply with
-	 * @param parse_mode - one of HTML, MarkdownV2, Markdown, or an empty string for ascii
-	 * @param options - any additional options to pass to sendMessage
-	 */
-	async reply(message: string, parse_mode = '', options: Record<string, number | string | boolean> = {}) {
-		switch (this.update_type) {
-			case 'message':
-				return await this.api.sendMessage(this.bot.api.toString(), {
-					...options,
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					reply_to_message_id: this.update.message?.message_id.toString() ?? '',
-					text: message,
-					parse_mode,
-				});
-			case 'business_message':
-				return await this.api.sendMessage(this.bot.api.toString(), {
-					chat_id: this.update.business_message?.chat.id.toString() ?? '',
-					text: message,
-					business_connection_id: this.update.business_message?.business_connection_id.toString(),
-					parse_mode,
-				});
-			case 'photo':
-				return await this.api.sendMessage(this.bot.api.toString(), {
-					...options,
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					reply_to_message_id: this.update.message?.message_id.toString() ?? '',
-					text: message,
-					parse_mode,
-				});
-			case 'inline':
-				return await this.api.answerInline(this.bot.api.toString(), {
-					inline_query_id: this.update.inline_query?.id.toString() ?? '',
-					results: [new TelegramInlineQueryResultArticle({ title: message, content: message, parse_mode })],
-				});
-			case 'document':
-				return await this.api.sendMessage(this.bot.api.toString(), {
-					...options,
-					chat_id: this.update.message?.chat.id.toString() ?? '',
-					reply_to_message_id: this.update.message?.message_id.toString() ?? '',
-					text: message,
-					parse_mode,
-				});
-			default:
-				break;
-		}
-	}
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * Send typing in a chat
+   * @returns Promise with the API response
+   */
+  async sendTyping() {
+    switch (this.update_type) {
+      case 'message':
+      case 'photo':
+      case 'document':
+        return await this.api.sendChatAction(this.bot.api.toString(), {
+          chat_id: this.getChatId(),
+          action: 'typing',
+        });
+      case 'business_message':
+        return await this.api.sendChatAction(this.bot.api.toString(), {
+          business_connection_id: this.update.business_message?.business_connection_id.toString() ?? '',
+          chat_id: this.getChatId(),
+          action: 'typing',
+        });
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * Reply to an inline message with a title and content
+   * @param title - title to reply with
+   * @param message - message contents to reply with
+   * @param parse_mode - parse mode to use
+   * @returns Promise with the API response
+   */
+  async replyInline(title: string, message: string, parse_mode = '') {
+    if (this.update_type === 'inline') {
+      return await this.api.answerInline(this.bot.api.toString(), {
+        inline_query_id: this.update.inline_query?.id.toString() ?? '',
+        results: [new TelegramInlineQueryResultArticle({ content: message, title, parse_mode })],
+      });
+    }
+    return null;
+  }
+
+  /**
+   * Reply to the last message with text
+   * @param message - text to reply with
+   * @param parse_mode - one of HTML, MarkdownV2, Markdown, or an empty string for ascii
+   * @param options - any additional options to pass to sendMessage
+   * @returns Promise with the API response
+   */
+  async reply(message: string, parse_mode = '', options: Record<string, number | string | boolean> = {}) {
+    switch (this.update_type) {
+      case 'message':
+      case 'photo':
+      case 'document':
+        return await this.api.sendMessage(this.bot.api.toString(), {
+          ...options,
+          chat_id: this.getChatId(),
+          reply_to_message_id: this.getMessageId(),
+          text: message,
+          parse_mode,
+        });
+      case 'business_message':
+        return await this.api.sendMessage(this.bot.api.toString(), {
+          chat_id: this.getChatId(),
+          text: message,
+          business_connection_id: this.update.business_message?.business_connection_id.toString() ?? '',
+          parse_mode,
+        });
+      case 'callback':
+        if (this.update.callback_query?.message.chat.id) {
+          return await this.api.sendMessage(this.bot.api.toString(), {
+            ...options,
+            chat_id: this.update.callback_query.message.chat.id.toString(),
+            text: message,
+            parse_mode,
+          });
+        }
+        return null;
+      case 'inline':
+        return await this.replyInline('Response', message, parse_mode);
+      default:
+        return null;
+    }
+  }
 }
